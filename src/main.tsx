7baa7abf63435aa5378b78000d659477df149cc4
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2025-04-09 18:48:30 +0400

    routing with 7.5.0

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,49 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux"; // Import the Provider from react-redux
-import { store } from "./store/store"; // Import your Redux store
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { Provider } from "react-redux"; // redux provider
+import { store } from "./store/store"; // redux store
 import "./index.css";
-import App from "./App.tsx";
 
+// იმპორტი კომპონენტების
+import Detail from "./pages/Detail";
+import NotFound from "./pages/NotFound";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Basket from "./pages/Basket";
+import Favorites from "./pages/Favorites";
+
+// მარშრუტების კონფიგურაცია
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Header /> {/* Header ყველა გვერდზე */}
+        <Outlet /> {/* შიგთავსი აქ ჩაიტვირთება */}
+        <Footer /> {/* Footer ყველა გვერდზე */}
+      </>
+    ),
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/detail", element: <Detail /> },
+      { path: "/category", element: <Category /> },
+      { path: "/favorites", element: <Favorites /> },
+      { path: "/basket", element: <Basket /> },
+      { path: "*", element: <NotFound /> }, // 404 გვერდი
+    ],
+  },
+]);
+
+// main.tsx -ში ყველაფერი ერთად
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      {/* Wrap the entire application in the Provider */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      {" "}
+      {/* Redux provider */}
+      <RouterProvider router={router} /> {/* Router system */}
     </Provider>
   </StrictMode>
 );
