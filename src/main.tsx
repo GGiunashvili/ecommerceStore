966447116bb9900e1cc4081d68f93e18c8d807ee
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2025-04-22 17:20:13 +0400

    scroll top on every page navigate

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import { Provider } from "react-redux"; // redux provider
-import { store } from "./store/store"; // redux store
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import "./index.css";
 
-// იმპორტი კომპონენტების
 import Detail from "./pages/Detail";
 import NotFound from "./pages/NotFound";
 import Footer from "./components/Footer";
@@ -13,41 +12,37 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
 import Basket from "./pages/Basket";
-// import Favorites from "./pages/Favorites";
+import ScrollToTop from "./components/ScrollToTop"; // ✅
 
-// მარშრუტების კონფიგურაცია
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <>
-        <Header /> {/* Header ყველა გვერდზე */}
+        <ScrollToTop /> {/* ✅ აქ არის სწორი ადგილი */}
+        <Header />
         <div className="min-h-[calc(100vh-356px)] mt-[148px] md:mt-[148px]">
-          <Outlet /> {/* შიგთავსი აქ ჩაიტვირთება */}
+          <Outlet />
         </div>
-        <Footer /> {/* Footer ყველა გვერდზე */}
+        <Footer />
       </>
     ),
     children: [
       { path: "/", element: <Home /> },
       { path: "/detail", element: <Detail /> },
       { path: "/detail/:id", element: <Detail /> },
-
       { path: "/category", element: <Category /> },
       { path: "/category/:id", element: <Category /> },
-
-      // { path: "/favorites", element: <Favorites /> },
       { path: "/basket", element: <Basket /> },
-      { path: "*", element: <NotFound /> }, // 404 გვერდი
+      { path: "*", element: <NotFound /> },
     ],
   },
 ]);
 
-// main.tsx -ში ყველაფერი ერთად
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} /> {/* Router system */}
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
